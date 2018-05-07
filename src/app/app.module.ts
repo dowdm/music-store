b8e81dc325ec53ee1abaff414f6bc59a3a56db0d
Brea Borlas and Matt Dowdney <brea@>
2018-05-07 11:26:55 -0700

    edits files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
 import { routing } from './app.routing';
+
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutComponent } from './about/about.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
 import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { AdminComponent } from './admin/admin.component';
+
 import { masterFirebaseConfig } from './api-keys';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AdminComponent } from './admin/admin.component';
+
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
